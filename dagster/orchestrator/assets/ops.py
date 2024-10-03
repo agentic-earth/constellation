be6d82ade15bf88d72c2120f394c14eaf0b1b051
John Finberg <johnbfinberg@gmail.com>
2024-10-03 17:47:52 -0400

    feat: dynamic graph and job creation

--- dagster/orchestrator/assets/ops.py
+++ dagster/orchestrator/assets/ops.py
@@ -1,38 +1,46 @@
-import operator
-from dagster import OpExecutionContext, asset, op
+from dagster import In, OpExecutionContext, Out, op
 import pandas as pd
+import operator
 
 
-@asset
-def mock_csv_data(ctx: OpExecutionContext) -> pd.DataFrame:
+@op(name="mock_csv_data", out=Out())
+def mock_csv_data(context: OpExecutionContext) -> pd.DataFrame:
     data = [["column1", "column2", "column3"], [1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    ctx.log.info(f"Mock data: {data}")
+    context.log.info(f"Mock data: {data}")
 
     df = pd.DataFrame(data[1:], columns=data[0])
-    ctx.log.info(f"DataFrame created from mock data:\n{df}")
+    context.log.info(f"DataFrame created from mock data:\n{df}")
 
     return df
 
 
-@asset
-def write_csv(ctx: OpExecutionContext, result: pd.DataFrame) -> str:
-    ctx.log.info(f"Received data to write to CSV:\n{result}")
+@op(name="write_csv", ins={"result": In(pd.DataFrame)}, out=Out(str))
+def write_csv(context: OpExecutionContext, result: pd.DataFrame) -> str:
+    context.log.info(f"Received data to write to CSV:\n{result}")
 
     result.to_csv("output.csv", index=False)
 
-    ctx.log.info(f"Data written to output.csv")
+    context.log.info(f"Data written to output.csv")
     return "output.csv"
 
 
-@op
+@op(
+    name="math_block",
+    ins={
+        "data": In(pd.DataFrame),
+        "operand": In(str),
+        "constant": In(float),
+    },
+    out=Out(pd.DataFrame),
+)
 def math_block(
-    ctx: OpExecutionContext,
+    context: OpExecutionContext,
     operand: str,
     constant: float,
     data: pd.DataFrame,
 ) -> pd.DataFrame:
     operand = getattr(operator, operand)
-    ctx.log.info(f"Applying math operation: {operand.__name__}")
+    context.log.info(f"Applying math operation: {operand.__name__}")
     result = data.apply(lambda x: operand(x, constant), axis=1)
-    ctx.log.info(f"Data after {operand.__name__} operation:\n{result}")
+    context.log.info(f"Data after {operand.__name__} operation:\n{result}")
     return result
