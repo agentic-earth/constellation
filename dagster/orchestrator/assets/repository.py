7fe3ec4bb44874c9f11630744f38ece9ecda3b19
John Finberg <johnbfinberg@gmail.com>
2024-10-03 17:47:52 -0400

    feat: dynamic graph and job creation

--- dagster/orchestrator/assets/repository.py
+++ dagster/orchestrator/assets/repository.py
@@ -1,15 +1,21 @@
 from dagster import (
+    Any,
+    DependencyDefinition,
+    DynamicOut,
+    DynamicOutput,
     Field,
-    Shape,
-    graph,
-    op,
+    GraphDefinition,
+    JobDefinition,
+    NodeInvocation,
+    job,
     repository,
-    OpExecutionContext,
 )
 from dataclasses import dataclass
 
 from orchestrator.assets.ops import *
 from orchestrator.assets.jobs import *
+
+OP_DEFS = [mock_csv_data, write_csv, math_block]
 
 
 @dataclass
@@ -18,119 +24,101 @@ class CallableOperation:
     parameters: dict[str, any]
 
 
-@op(
-    config_schema=Shape(
-        {
-            "instructions": Field(
-                Shape(
-                    {
-                        "operation": Field(
-                            str,
-                            description="Operation to be performed",
-                            default_value="write_csv",
-                        ),
-                        "parameters": Field(
-                            dict,
-                            description="Parameters required for the operation",
-                            default_value={
-                                "result": {
-                                    "operation": "math_block",
-                                    "parameters": {
-                                        "operand": "add",
-                                        "constant": 10.0,
-                                        "data": {"operation": "mock_csv_data"},
-                                    },
-                                }
-                            },
-                        ),
-                    }
-                ),
-                description="Instructions to build the job",
-                is_required=True,
-            )
-        }
-    )
-)
-def job_assembler(context: OpExecutionContext) -> CallableOperation:
-    context.log.info("Parsing instructions to create job")
-
-    def parse_parameters(params):
-        parsed_params = {}
-        for key, value in params.items():
-            if isinstance(value, dict) and "operation" in value:
-                _ = globals()[value["operation"]]
-                nested_params = value.get("parameters", {})
-
-                context.log.info(f"Nested job: {value['operation']}")
-                parsed_params[key] = CallableOperation(
-                    value["operation"], parse_parameters(nested_params)
-                )
-            else:
-                parsed_params[key] = value
-        return parsed_params
-
-    instruction = context.op_config["instructions"]
-    if "operation" not in instruction:
+def parse_instructions(input: dict) -> CallableOperation:
+    if "operation" not in input:
         raise ValueError("Operation not found in instruction")
-    _ = globals()[instruction["operation"]]
 
+    # FIXME: Do some check to make sure exists
+
+    operation = input["operation"]
     parameters = {}
-    if "parameters" in instruction:
-        parameters = parse_parameters(instruction["parameters"])
 
-    context.log.info(f"Instruction parsed: {instruction['operation']}")
+    for key, value in input.get("parameters", {}).items():
+        if isinstance(value, dict) and "operation" in value:
+            parameters[key] = parse_instructions(value)
 
-    return CallableOperation(instruction["operation"], parameters)
+    return CallableOperation(operation=operation, parameters=parameters)
+
+
+def generate_dependencies(instruction: CallableOperation, deps: dict) -> dict:
+    result = {}
+    alias_counter = [1]
+
+    def traverse(node):
+        alias = f"{node.operation} ({alias_counter[0]})"
+        alias_counter[0] += 1
+
+        current_node = NodeInvocation(name=node.operation, alias=alias)
+
+        dependencies = {}
+        for key, param_node in node.parameters.items():
+            param_alias = traverse(param_node)
+            dependencies[key] = DependencyDefinition(param_alias)
+
+        result[current_node] = dependencies if dependencies else {}
+        return alias
+
+    traverse(instruction)
+    return result
+
+
+# TODO: Return both GraphDeinition and run_config
+def define_composite_job(name: str, raw_input: dict) -> GraphDefinition:
+    instruction: CallableOperation = parse_instructions(input=raw_input)
+    deps: dict = generate_dependencies(instruction=instruction, deps={})
+    print(deps)
+
+    return GraphDefinition(
+        name=name,
+        node_defs=OP_DEFS,
+        dependencies=deps,
+    ).to_job()
+
+
+@op(config_schema={"raw_input": Field(Any)}, out=DynamicOut())
+def generate_dynamic_job_configs(context: OpExecutionContext):
+    raw_input = context.op_config["raw_input"]
+    yield DynamicOutput(raw_input, mapping_key="dynamic_config")
 
 
 @op
-def job_executer(context: OpExecutionContext, operations: CallableOperation) -> None:
-    context.log.info("Executing job operations")
+def parse_and_execute_job(context: OpExecutionContext, raw_input: dict):
+    # Dynamically generate a job based on the input
+    job = define_composite_job(name="dynamic_job", raw_input=raw_input)
+    context.log.info(f"Created dynamic job: {job.name}")
 
-    def execute_helper(operations):
-        context.log.info(f"Executing operation: {operations.operation}")
+    context.log.info(f"Executing dynamic job")
 
-        if operations.operation not in globals():
-            raise ValueError(f"Operation {operations.operation} not found")
+    # TODO: Make run_config dynamic
+    result = job.execute_in_process(
+        run_config={
+            "ops": {"math_block (2)": {"inputs": {"constant": 0.0, "operand": "add"}}}
+        }
+    )
 
-        computed_params = {}
-        if operations.parameters:
-            for key, value in operations.parameters.items():
-                if isinstance(value, CallableOperation):
-                    result = execute_helper(value)
-                    computed_params[key] = result
-                else:
-                    computed_params[key] = value
+    for event in result.all_events:
+        context.log.info(f"Event: {event.message}")
 
-        context.log.info(f"Executing operation: {operations.operation}")
-        fn = globals()[operations.operation]
-        return fn(ctx=context, **computed_params)
-
-    execute_helper(operations)
-
-    context.log.info("Job operations executed")
+    context.log.info(f"Dynamic job result: {result.output_value()}")
 
 
-@graph()
-def dynamic_math():
-    instructions = job_assembler()
-    job_executer(operations=instructions)
-
-
-dynamic_math_job = dynamic_math.to_job(
+@job(
     config={
         "ops": {
-            "job_assembler": {
+            "generate_dynamic_job_configs": {
                 "config": {
-                    "instructions": {
+                    "raw_input": {
                         "operation": "write_csv",
                         "parameters": {
                             "result": {
                                 "operation": "math_block",
                                 "parameters": {
+                                    "data": {
+                                        "operation": "mock_csv_data",
+                                        "parameters": {},
+                                    },
                                     "operand": "add",
-                                    "constant": 10.0,
-                                    "data": {"operation": "mock_csv_data"},
+                                    "constant": 5,
                                 },
                             },
                         },
@@ -140,11 +128,11 @@ dynamic_math_job = dynamic_math.to_job(
         }
     }
 )
-
-
-dynamic_math_job = dynamic_math.to_job()
+def build_execute_job():
+    dynamic_configs = generate_dynamic_job_configs()
+    dynamic_configs.map(parse_and_execute_job)
 
 
 @repository
 def deploy_docker_repository():
-    return [dynamic_math_job]
+    return [build_execute_job]
