c9e80d14678b50ff3a4e09155ddb59a47da3c190
masonlee277 <masonlee27@icloud.com>
2024-10-09 23:23:24 +0000

    user service with supabase auth

--- api/backend/app/database.py
+++ api/backend/app/database.py
@@ -4,8 +4,10 @@ from prisma import Prisma
 from backend.app.logger import ConstellationLogger
 from backend.app.config import settings
 import traceback
+from supabase import create_client, Client
 
 prisma_client = Prisma(datasource={"url": str(settings.DATABASE_URL)})
+supabase_client: Client = None
 
 logger = ConstellationLogger()
 
@@ -46,3 +48,33 @@ async def disconnect_db():
             f"Failed to disconnect Prisma Client: {str(e)}",
             extra={"traceback": traceback.format_exc()}
         )
+
+def connect_supabase():
+    global supabase_client
+    try:
+        logger.log(
+            "Database",
+            "info",
+            f"Attempting to connect to Supabase with URL: {settings.SUPABASE_URL}"
+        )
+        supabase_client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+        logger.log(
+            "Database",
+            "info",
+            "Supabase Client connected successfully."
+        )
+        return supabase_client
+    except Exception as e:
+        logger.log(
+            "Database",
+            "critical",
+            f"Failed to connect Supabase Client: {str(e)}",
+            extra={"traceback": traceback.format_exc()}
+        )
+        raise
+
+def get_supabase_client() -> Client:
+    global supabase_client
+    if supabase_client is None:
+        supabase_client = connect_supabase()
+    return supabase_client
