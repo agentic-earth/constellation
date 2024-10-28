b4b16a52743d6c567f8311208e2d2b972ae5c60d
wdorji <wangdrak.dorji@gmail.com>
2024-10-28 17:33:06 -0400

    Get service name directly

--- modal/modal_creator/main.py
+++ modal/modal_creator/main.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI
-from modal_creator.utils import deploy_model_service, delete_model_service, post_model_inference
+from modal_creator.utils import deploy_model_service, delete_model_service, post_model_inference, get_service_code
 
 app = FastAPI()
-services_map = {}
-
 
 @app.post("/deploy")
 async def deploy(model_name: str):
     output = deploy_model_service(model_name)
-    services_map[model_name] = output["service_name"]
     return output
 
 
 @app.post("/infer")
 async def infer(model_name: str, data: dict):
-    service_name = services_map[model_name]
+
+    service_name = get_service_code(model_name)
     output = post_model_inference(service_name, data)
     return output
 
@@ -22,5 +20,4 @@ async def infer(model_name: str, data: dict):
 @app.post("/delete")
 async def delete(model_name: str):
     output = delete_model_service(model_name)
-    del services_map[model_name]
     return output
