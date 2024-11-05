339d8e30d24fd75584ef861084c6e8d644297b76
xulong xiao <justinxiaoxulong@gmail.com>
2024-11-05 16:34:31 -0500

    update to make research agent work

--- api/backend/app/features/agent/crews/research_crew.py
+++ api/backend/app/features/agent/crews/research_crew.py
@@ -1,42 +1,50 @@
-from crewai import Crew, Agent, Process, Task
-from crewai.project import CrewBase, agent, crew, task
-from langchain_openai import ChatOpenAI
 from backend.app.features.agent.tools.vector_embed_tool import VectorEmbedTool
 from backend.app.features.agent.tools.similarity_search_tool import SimilaritySearchTool
+from crewai import Crew, Agent, Process, Task
+from langchain_openai import ChatOpenAI
+import yaml
 
-@CrewBase
 class ResearchCrew:
     '''Research Crew'''
 
-    agents_config = "agents.yaml"
-    tasks_config = "tasks.yaml"
-    llm = ChatOpenAI(model="gpt-4o")
+    def __init__(self):
+        with open("C:/Users/JXPARATROOPER/Desktop/CourseCode/CSCI 2340/constellation-backend/api/backend/app/features/agent/config/agents.yaml", "r") as file:
+            self.agents_config = yaml.safe_load(file)
+        with open("C:/Users/JXPARATROOPER/Desktop/CourseCode/CSCI 2340/constellation-backend/api/backend/app/features/agent/config/tasks.yaml", "r") as file:
+            self.tasks_config = yaml.safe_load(file)
+        self.llm = ChatOpenAI(model="gpt-4o")
+        self.research_agent_ = self.research_agent()
+        self.research_task_ = self.research_task()
 
-    @agent
     def research_agent(self) -> Agent:
-
         return Agent(
-            config=self.agents_config["research_agent"],
+            config=self.agents_config["Researcher_agent"],
             tools=[
-                VectorEmbedTool(),
-                SimilaritySearchTool(),
+                SimilaritySearchTool,
+                VectorEmbedTool,
             ],
             llm=self.llm,
             verbose=True,
         )
     
-    @task
     def research_task(self) -> Task:
         return Task(
-            config=self.tasks_config["find_similar_papers_task"],
-            agent=self.research_agent,
+            # config=self.tasks_config["find_similar_paper"],
+            description="Find the most similar paper to the user's query and provide a summary.",
+            prompt_context='You are a researcher looking for similar papers to a given query.',
+            agent=self.research_agent_,
             process=Process.sequential,
+            expected_output="JSON string of similar papers",
+            verbose=True
         )
     
-    @crew
     def create_research_crew(self) -> Crew:
         return Crew(
-            agents=[self.research_agent],
-            tasks=[self.research_task],
-            verbose=2,
+            agents=[self.research_agent_],
+            tasks=[self.research_task_],
+            verbose=True,
         )
+    
+if __name__ == "__main__":
+    crew = ResearchCrew()
+    crew.create_research_crew().kickoff()
